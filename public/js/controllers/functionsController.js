d32bca5bad38621c030b63f0530c519aaf70b92f
sanxoom <mizan_01340@yahoo.com>
2013-11-11 01:27:15 +0600

    Create tab

--- public/js/controllers/functionsController.js
+++ public/js/controllers/functionsController.js
@@ -5,11 +5,20 @@
 function functionsController($scope, $routeParams, $location, Global, tabService) {
 
     $scope.IsAddTabButtonVisible = true;
+    $scope.userTabs = [];
+    $scope.menuTabs = [];
+
     var showTabs =function()
     {
         tabService.query(function(tabs) {
             $scope.tabs = tabs;
-            console.log($scope.tabs);
+            $scope.userTabs = _.where(tabs, {user : "*"});
+            $scope.menuTabs = _.where(tabs, {optional : true});
+
+           // angular.forEach($scope.menuTabs, function(menuTab, key)
+           // {
+             //   menuTab.isSelected = true;
+           // });
         });
     };
 
@@ -19,6 +28,32 @@ function functionsController($scope, $routeParams, $location, Global, tabService
     {
         $scope.IsAddTabButtonVisible = false;
     }
+
+    $scope.dropdownOk = function()
+    {
+        angular.forEach($scope.menuTabs, function(menuTab, key)
+        {
+            if(menuTab.isSelected)
+            {
+                var tabs = _.where($scope.userTabs, {name : menuTab.name});
+                if(tabs.length<=0)
+                {
+                    $scope.userTabs.push(menuTab);
+                }
+            }
+            else
+            {
+               $scope.userTabs = _.without($scope.userTabs, _.findWhere($scope.userTabs, {name: menuTab.name}));
+            }
+        });
+
+        $('[data-toggle="dropdown"]').parent().removeClass('open');
+    };
+
+    $scope.dropdownCancel = function()
+    {
+        $('[data-toggle="dropdown"]').parent().removeClass('open');
+    };
 
     $scope.saveTab = function()
     {
