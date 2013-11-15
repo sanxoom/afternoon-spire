3e84f323f5385f95673079a8316832201c7c7c6a
sanxoom <mizan_01340@yahoo.com>
2013-11-15 21:02:46 +0600

    Create tab

--- public/js/controllers/functionsController.js
+++ public/js/controllers/functionsController.js
@@ -73,4 +73,17 @@ function functionsController($scope, $routeParams, $location, Global, tabService
             $scope.IsAddTabButtonVisible = true;
         });
     }
+
+
+    //Grid
+    $scope.myData = [{name: "Moroni", age: 50},
+        {name: "Tiancum", age: 43},
+        {name: "Jacob", age: 27},
+        {name: "Nephi", age: 29},
+        {name: "Enos", age: 34}];
+    $scope.gridOptions = {
+        data: 'myData',
+        showGroupPanel: true
+    };
+
 }
